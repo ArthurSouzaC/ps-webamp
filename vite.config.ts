777113701b4347b21cf53f1134080da7d558ccb6
ArthurSouzaC <arthursouza.info@gmail.com>
2025-11-05 20:22:03 -0300

    fix: fixing gh pages build

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ps-webamp/",
   build: {
-    outDir: "docs",
     emptyOutDir: true,
   },
 });
